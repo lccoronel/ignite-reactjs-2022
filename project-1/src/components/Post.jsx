fbac21b86b3993e7593f16f896bb9d47108bd791
Lucas Coronel <lccoronel7@icloud.com>
2022-08-07 09:53:31 -0300

    ✨ post component

---
+++ project-1/src/components/Post.jsx
@@ -0,0 +1,45 @@
+import styles from './Post.module.css'
+
+export function Post() {
+   return (
+      <article className={styles.post}>
+         <header>
+            <div className={styles.author}>
+               <img 
+                  className={styles.avatar} 
+                  src="https://avatars.githubusercontent.com/u/54275445?v=4" 
+               />
+
+               <div>
+                  <strong>Lucas Coronel</strong>
+                  <span>Web developer</span>
+               </div>
+            </div>
+
+            <time 
+               title='11 de maio as 08:13' 
+               dateTime='2022-05-11 08:13:00'
+            >
+               Publicado a 1h
+            </time>
+         </header>
+
+         <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+
+            <p>
+               Acabei de subir mais um projeto no meu portifa. É um projeto que 
+               fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+            </p>
+
+            <p>
+               <a>👉 jane.design/doctorcare</a>
+            </p>
+
+            <p>
+               <a>#novoprojeto #nlw #rocketseat</a>
+            </p>
+         </div>
+      </article>
+   )
+}
