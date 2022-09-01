28357334760f572b18b216fa0299dc8f7d0c312e
Lucas Coronel <lccoronel7@icloud.com>
2022-09-01 19:39:07 -0300

    ♻️ create task button

--- desafio-1/src/App.jsx
+++ desafio-1/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import logoImg from './assets/Logo.png'
+import plusImg from './assets/plus.png'
 import styles from './App.module.css'
 import './global.css'
 
@@ -10,11 +11,13 @@ function App() {
       <header>
         <img src={logoImg} alt="logo" />
       </header>
+
       <body>
         <form>
           <input type="text" placeholder='Adicione uma nova tarefa' />
           <button type="submit">
             Criar
+            <img src={plusImg} alt="adicionar" />
           </button>
         </form>
       </body>
