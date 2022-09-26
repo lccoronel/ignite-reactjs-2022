278f2d42d29a6b3cd6cc69e5d422af2f28942520
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-09-26 17:45:27 -0300

    ✨ setup global styles

--- project-2/src/styles/global.ts
+++ project-2/src/styles/global.ts
@@ -7,8 +7,19 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
-    body {
-        background-color: #333;
-        color: #000;
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${({theme}) => theme["green-500"]};
     }
+
+    body {
+        background-color: ${({theme}) => theme["gray-900"]};
+        color: ${({theme}) => theme["gray-300"]};
+    }
+
+    body, input, text-area, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    } 
 `
